3f77be7807c5117a57bc7040ca34ebaefcac9424
Gonzalo Diaz Mizzau <gonzajd777@gmail.com>
2025-01-30 22:40:21 -0300

    30/01/2025

--- ProyectoFinal-PrimeraEntrega/src/main/java/com/coderhouse/ProyectoFinal_PrimeraEntrega/controller/SellingController.java
+++ ProyectoFinal-PrimeraEntrega/src/main/java/com/coderhouse/ProyectoFinal_PrimeraEntrega/controller/SellingController.java
@@ -9,11 +9,14 @@ import com.coderhouse.ProyectoFinal_PrimeraEntrega.mapper.CartMapper;
 import com.coderhouse.ProyectoFinal_PrimeraEntrega.mapper.ProductMapper;
 import com.coderhouse.ProyectoFinal_PrimeraEntrega.model.Client;
 import com.coderhouse.ProyectoFinal_PrimeraEntrega.model.ErrorType;
+import com.coderhouse.ProyectoFinal_PrimeraEntrega.request.SellingApiRequest;
 import com.coderhouse.ProyectoFinal_PrimeraEntrega.response.ApiResponse;
 import com.coderhouse.ProyectoFinal_PrimeraEntrega.service.CartService;
 import com.coderhouse.ProyectoFinal_PrimeraEntrega.service.ClientService;
 import com.coderhouse.ProyectoFinal_PrimeraEntrega.service.TicketService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -36,9 +39,15 @@ public class SellingController {
     @Autowired
     ClientService mClientService;
 
-    @Operation(summary = "Endpoint de Venta de productos",
-            description = "Este endpoint toma el request con el formato especificado y sirviendose de la logica y validaciones de CartService y TicketServive, agrega los productos al carrito y luego genera el comprobante para la venta, realizando validaciones sobre Cliente, Producto, reduciendo Stock, registrando errores que puedan surgir e informando en la respuesta el resultado.<br>" +
-                    "Como se menciono hace uso de servicios los cuales incluyes validaciones encapsuladas de cada entidad interviniente en el proceso, por lo que ante un error en la logica de negocio se registrara el error en al respuesta.")
+    @Operation(summary = "Endpoint de Venta de productos"
+            ,description = "Este endpoint toma el request con el formato especificado y sirviendose de la logica y validaciones de CartService y TicketServive, agrega los productos al carrito y luego genera el comprobante para la venta, realizando validaciones sobre Cliente, Producto, reduciendo Stock, registrando errores que puedan surgir e informando en la respuesta el resultado.<br>" +
+                    "Como se menciono hace uso de servicios los cuales incluyes validaciones encapsuladas de cada entidad interviniente en el proceso, por lo que ante un error en la logica de negocio se registrara el error en al respuesta."
+            ,requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(  content = @Content( schema = @Schema(implementation = SellingApiRequest.class)  ) )
+            ,responses = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Pedido creado exitosamente"),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Error en la request",content = @Content(schema = @Schema(implementation = ApiResponse.class)) ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Error en el servicio",content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+    })
     @PostMapping
     public ResponseEntity<ApiResponse<TicketExtendedDTO>> createSale(@RequestBody Map<String, Object> pRequestBody) {
         try {
@@ -67,10 +76,11 @@ public class SellingController {
             }
             TicketExtendedDTO mTicketExtendedDTO = new TicketExtendedDTO();
             try {
-            mTicketExtendedDTO = mTicketService.createTicket(mClientId);
+                mTicketExtendedDTO = mTicketService.createTicket(mClientId);
             } catch (CustomException ce){
                 mApiResponse.addError(ce.getCustomExceptionText());
             }
+            mApiResponse.setSuccess(true);
             mApiResponse.setMessage("El Ticket se genero correctamente.");
             mApiResponse.setData(mTicketExtendedDTO);
 
@@ -79,9 +89,9 @@ public class SellingController {
                 mApiResponse.setMessage("Ticket couldn't be generated, none of the items in the client's cart fulfill the requirements.");
             }
             if (mTicketExtendedDTO.getNotEnoughStockProducts() !=null){
-            if (!mTicketExtendedDTO.getNotEnoughStockProducts().isEmpty()) {
-                mApiResponse.addError("Some items could not be sold because there is not enough stock. See notEnoughStockProducts");
-            }}
+                if (!mTicketExtendedDTO.getNotEnoughStockProducts().isEmpty()) {
+                    mApiResponse.addError("Some items could not be sold because there is not enough stock. See notEnoughStockProducts");
+                }}
 
             return ResponseEntity.status(HttpStatus.OK).body(mApiResponse);
         } catch (CustomException e) {
---
+++ ProyectoFinal-PrimeraEntrega/src/main/java/com/coderhouse/ProyectoFinal_PrimeraEntrega/request/SellingApiRequest.java
@@ -0,0 +1,78 @@
+package com.coderhouse.ProyectoFinal_PrimeraEntrega.request;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+
+@Schema(description = "Request model for creating a sale")
+public class SellingApiRequest {
+
+    @Schema(description = "ID del cliente", example = "25")
+    private Long cliente;
+
+    @Schema(description = "Detalle del carrito")
+    private List<ItemDetalleCarrito> detalleCarrito;
+
+    public SellingApiRequest() {}
+
+    @JsonCreator
+    public SellingApiRequest(@JsonProperty("cliente") Long cliente,
+                             @JsonProperty("detalleCarrito") List<ItemDetalleCarrito> detalleCarrito) {
+        this.cliente = cliente;
+        this.detalleCarrito = detalleCarrito;
+    }
+
+    // Getters y setters
+    public Long getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Long cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ItemDetalleCarrito> getDetalleCarrito() {
+        return detalleCarrito;
+    }
+
+    public void setDetalleCarrito(List<ItemDetalleCarrito> detalleCarrito) {
+        this.detalleCarrito = detalleCarrito;
+    }
+
+    @Schema(description = "Detalle del carrito")
+    public static class ItemDetalleCarrito {
+
+        @Schema(description = "ID del producto", example = "155")
+        private Long producto;
+
+        @Schema(description = "Cantidad de producto", example = "6")
+        private Integer cantidad;
+
+        public ItemDetalleCarrito() {}
+
+        @JsonCreator
+        public ItemDetalleCarrito(@JsonProperty("producto") Long producto,
+                                  @JsonProperty("cantidad") Integer cantidad) {
+            this.producto = producto;
+            this.cantidad = cantidad;
+        }
+
+        // Getters y setters
+        public Long getProducto() {
+            return producto;
+        }
+
+        public void setProducto(Long producto) {
+            this.producto = producto;
+        }
+
+        public Integer getCantidad() {
+            return cantidad;
+        }
+
+        public void setCantidad(Integer cantidad) {
+            this.cantidad = cantidad;
+        }
+    }
+}
